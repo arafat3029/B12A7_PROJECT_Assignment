7a0f163abec90ec7738487df81ec0d53571f264e
arafat3029 <mdarafat12890@gmail.com>
2026-01-13 22:21:34 +0600

    never completed

--- src/assets/Comonents/Navber/Navber.jsx
+++ src/assets/Comonents/Navber/Navber.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Navber = () => {
   return (
     <div className="shadow-sm">
-      <div className="navbar  flex justify-between items-center p-4 max-w-7xl mx-auto">
+      <div className="navbar flex flex-col md:flex-row xl:flex-wrap justify-between items-center p-4 max-w-7xl mx-auto ">
         <div className="flex-1">
           <a className=" btn-ghost text-xl font-bold text-[#130B2D]">CS — Ticket System</a>
         </div>
-        <div className="flex-none">
+        <div className=" flex-none">
           <button className="ml-5 text-[#1a1a1a]">Home</button>
           <button className="ml-5 text-[#1a1a1a]">FAQ</button>
           <button className="ml-5 text-[#1a1a1a]">Changelog</button>
