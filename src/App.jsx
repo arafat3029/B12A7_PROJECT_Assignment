b157e02d6f86695a4b2a90b6fc82eea7582d8458
arafat3029 <mdarafat12890@gmail.com>
2026-01-13 21:32:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navber from './assets/Comonents/Navber/Navber'
+
+function App() {
+
+  return (
+    <>
+      <Navber></Navber>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Comonents/Navber/Navber.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navber = () => {
+  return (
+    <div className="shadow-sm">
+      <div className="navbar  flex justify-between items-center p-4 max-w-7xl mx-auto">
+        <div className="flex-1">
+          <a className=" btn-ghost text-xl font-bold text-[#130B2D]">CS — Ticket System</a>
+        </div>
+        <div className="flex-none">
+          <button className="ml-5 text-[#1a1a1a]">Home</button>
+          <button className="ml-5 text-[#1a1a1a]">FAQ</button>
+          <button className="ml-5 text-[#1a1a1a]">Changelog</button>
+          <button className="ml-5 text-[#1a1a1a]">Blog</button>
+          <button className="ml-5 text-[#1a1a1a]">Download</button>
+          <button className="ml-5 text-[#1a1a1a]">Contact</button>
+          <button className="btn rounded-b-md ml-5 text-[#fff] bg-gradient-to-r from-[#632EE3] to-[#9F62F2]"> <span>+</span> Contact</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
